1ca0afb67c8dd1ef8ed30f2579005c709eb7a765
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-02-05 23:27:03 +0000

    Update Kernels.jl

--- src/Kernels/Kernels.jl
+++ src/Kernels/Kernels.jl
@@ -1,6 +1,4 @@
-"
-This defines kernel in addition to BaytesMCMC, BaytesFilters and BaytesPMCMC.
-"
+"This defines kernel in addition to BaytesMCMC, BaytesFilters and BaytesPMCMC."
 ################################################################################
 #Import
 include("SMC2/SMC2.jl")
