4eaf6491da058fca0e9465bc5282853cab4429d1
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-04-04 11:35:10 +0100

    Update tests

--- test/TestHelper.jl
+++ test/TestHelper.jl
@@ -12,16 +12,16 @@ N = 10^3
 
 ############################################################################################
 # Kernel and AD backends
-data_uv = randn(_rng, 1000)
 
 ############################################################################################
 # Initiate Base Model to check sampler
 
 ######################################## Model 1
 struct MyBaseModel <: ModelName end
-myparameter = (μ = Param(0.0, Normal()), σ = Param(1.0, Gamma()))
+myparameter = (μ = Param(0.0, Normal()), σ = Param(10.0, Gamma()))
 mymodel = ModelWrapper(MyBaseModel(), myparameter)
 
+data_uv = rand(_rng, Normal(mymodel.val.μ, mymodel.val.σ), 1000)
 #Create objective for both μ and σ and define a target function for it
 myobjective = Objective(mymodel, data_uv, (:μ, :σ))
 function (objective::Objective{<:ModelWrapper{MyBaseModel}})(θ::NamedTuple)
--- test/test-construction.jl
+++ test/test-construction.jl
@@ -10,8 +10,9 @@ objectives = [
 for iter in eachindex(objectives)
     _obj = objectives[iter]
     _flattentype = _obj.model.info.flattendefault.output
-    @testset "Kernel construction and propagation, MCMC" begin
+    @testset "Kernel construction and propagation, ASMC" begin
         ## Assign smc kernel ~ use PMCMC to check both MCMC and Particle Filter
+
         ## MCMC
         mcmc = MCMC(NUTS,(:μ, :σ,); stepsize = ConfigStepsize(;stepsizeadaption = UpdateFalse()))
         kerneldefault  = SMCDefault(Ntuning = 5, jittermin = 1, jittermax = 5)
@@ -31,11 +32,46 @@ for iter in eachindex(objectives)
             _, diags[iter] = propose!(_rng, smc, _obj.model, _obj.data)
         end
         results(diags, smc, 2, [.1, .2, .5, .8, .9])
+
+        #PMCMC
+        
     end
 end
 
 ############################################################################################
 ## Check IBIS
+for iter in eachindex(objectives)
+    _obj = objectives[iter]
+    _flattentype = _obj.model.info.flattendefault.output
+    @testset "Kernel construction and propagation, IBIS" begin
+        ## Assign smc kernel ~ use PMCMC to check both MCMC and Particle Filter
+        ## MCMC
+        mcmc = MCMC(NUTS,(:μ, :σ,); stepsize = ConfigStepsize(;stepsizeadaption = UpdateFalse()))
+        kerneldefault  = SMCDefault(Ntuning = 5, jittermin = 1, jittermax = 5)
+        samplingdefault = SampleDefault(chains = 4)
+        smc_c = SMCConstructor(mcmc, SMCDefault())
+
+        #Create new objective with small amount of data
+        _obj2 = Objective(_obj.model, _obj.data[1:50])
+        SMC(_rng, mcmc, _obj2, kerneldefault, samplingdefault)
+        @test BaytesCore.get_sym(mcmc) == BaytesSMC.get_sym(smc_c)
+        ## Propose new parameter
+        smc = SMC(_rng, mcmc, _obj2)
+        vals, diagnostics = propose!(_rng, smc, _obj2.model, _obj2.data)
+        @test eltype(diagnostics.ρ) == _flattentype
+        ## Postprocessing
+        smc = SMC(_rng, mcmc, _obj2)
+        diagtype = infer(_rng, AbstractDiagnostics, smc, _obj.model, _obj.data)
+        @test diagnostics isa diagtype
+        datadiff = length(_obj.data) - length(_obj2.data) - 1
+        diags = Vector{diagtype}(undef, datadiff)
+        for iter in eachindex(diags)
+            data_temp = _obj.data[1:(length(_obj2.data) + iter)]
+            _, diags[iter] = propose!(_rng, smc, _obj.model, data_temp)
+        end
+        results(diags, smc, 2, [.1, .2, .5, .8, .9])
+    end
+end
 
 ############################################################################################
 ## Check SMC2
